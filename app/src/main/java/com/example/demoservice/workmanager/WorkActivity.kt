a53da1f5e18449dcac0f8f423d76f3b831c4dbab
khoa-qh2470 <quach.hong.khoa@sun-asterisk.com>
2021-04-02 13:49:44 +0700

    add

--- app/src/main/java/com/example/demoservice/workmanager/WorkActivity.kt
+++ app/src/main/java/com/example/demoservice/workmanager/WorkActivity.kt
@@ -1,13 +1,12 @@
 package com.example.demoservice.workmanager
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.demoservice.R
 import kotlinx.android.synthetic.main.activity_work.*
+import java.util.concurrent.TimeUnit
 
 class WorkActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +27,10 @@ class WorkActivity : AppCompatActivity() {
             WorkManager
                 .getInstance(this)
                 .enqueue(myWork)
+
+//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//                PeriodicWorkRequest.Builder(MyWork::class.java, 100000, TimeUnit.MILLISECONDS)
+//            }
 
         }
 
